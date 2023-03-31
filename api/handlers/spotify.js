a8154af2c4c33032d1bcea2407f01000f5c259cf
Juan Carlos Asensio <jasensio@ufm.edu>
2023-03-31 11:02:07 -0400

    Handle token refresh and fetch retries --wip

--- api/handlers/spotify.js
+++ api/handlers/spotify.js
@@ -2,27 +2,74 @@ const fetch = require('node-fetch');
 const processErrorResponse = require('../utils/processErrorResponse.js');
 const { filterEmptyURL, sortByDate } = require('../utils/hnDataFilters');
 
+
+let token = {
+  'value': '',
+  'issued_at': ''
+}
+
 const getArtist = async (req, res) => {
   const { query } = req.params;
+  let currentToken = token.value;
   console.log("You've hit /api/spotify/artist with query: ", query)
   
   try {
     const URL = `https://api.spotify.com/v1/search?query=${encodeURIComponent(query)}&type=artist&tag:hipster`
     console.log('This is the URL, lads', URL)
-    
-    const response = await fetch(URL, {
-      headers: {
-        "Authorization": 'Bearer  BQAUpf_PzvIsGFdRKpvaOUcr70GIpX0ufvlhk9pSeeya4kl4YLvSIuAiZoJxa0B60pNkxMsqYdDZqlDvrtXTfAl9BdwbQ6--tEYODED2A_pWfMQjOgUj'
-      },
-    });
 
-    const data = await response.json();
-    console.log("data", data);
+    let response = await getSpotifySearch(URL, currentToken)
+    let data = await response.json();
+
+    
+    //TODO: find a better way to handle retries? 
+    // https://markmichon.com/automatic-retries-with-fetch
+    while (data?.error) {
+      if (data.error.status === 401) {
+        token = getSpotifyAccessToken();
+        response = await getSpotifySearch(URL, currentToken)
+        data = await response.json();
+      }
+    }
+
     res.status(200).json(data.artists)
   } catch (error) {  
     let errMessage = `${error}`;
+    console.log("There was an error", errMessage);
     processErrorResponse(res, 500, errMessage);  
   }
+}
+
+// Can I "save" the last token (or time of last token) between calls to Spotify using closure?
+const getSpotifySearch = async (url, token) => {  
+  try {
+    const response = await fetch(URL, {
+      headers: {
+          "Authorization": `Bearer  ${token}`
+        },
+      });
+    return response
+  } catch (error) {
+    throw new Error(`${error.message}`);
+  }
+}
+
+  //TODO: um, this is a problem. https://developer.spotify.com/documentation/web-api/tutorials/getting-started#request-artist-data
+  // Tokens are only valid for 1 hour :shrug
+const getSpotifyAccessToken = async() => {
+  try {
+    const tokenResponse = await fetch("https://accounts.spotify.com/api/token", {
+      method : 'POST',
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `grant_type=client_credentials&client_id=${process.env.SPOTIFY_CLIENT_ID}&client_secret=${process.env.SPOTIFY_CLIENT_SECRET}`
+    });
+    
+    const token = await tokenResponse.json().access_token;
+    return token
+  } catch (error) {
+    throw new Error(`${error.message}`);
+  }  
 }
 
 const getArticlesByQuery = async (req, res) => {
