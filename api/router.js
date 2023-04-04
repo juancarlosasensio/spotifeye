59b2e9287605f9f005e621e3d00350306a7e5c56
Juan Carlos Asensio <jasensio@ufm.edu>
2023-04-04 08:42:32 -0400

    Cleanup unused code

--- api/router.js
+++ api/router.js
@@ -1,14 +1,8 @@
 const { Router } = require('express');
-const { 
-  getArticlesByQuery, 
-  getFrontPageArticles 
-} = require('./handlers/hackerNews');
 const { getArtists, getTracks } = require('./handlers/spotify')
 
 const router = Router();
 
-router.get('/hn', getFrontPageArticles)
-router.get('/hn/:query', getArticlesByQuery)
 router.get('/spotify/search/artists/:query', getArtists)
 router.get('/spotify/search/tracks/:query', getTracks)
 
